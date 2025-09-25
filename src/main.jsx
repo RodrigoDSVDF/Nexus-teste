798e6bb218ba33e121319e0278a82a0ff93f41af
Rodrigo <76666085+RodrigoDSVDF@users.noreply.github.com>
2025-09-25 09:34:59 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+// O CÓDIGO DEVE FICAR ASSIM (CORRETO)
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* A única mudança é adicionar a propriedade 'basename' aqui */}
-    <BrowserRouter basename="/Nexus-teste">
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter> {/* <--- REMOVA O BASENAME DAQUI */}
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>,
 )
